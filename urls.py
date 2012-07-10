3dcd1414488cec0b6b5c7dd472e615c5abc322e5
Josh Mandel <Joshua.Mandel@childrens.harvard.edu>
2012-07-09 17:54:32 -0700

    Separate ui templates from admin_ui templates

--- urls.py
+++ urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
-urlpatterns = patterns('',
-    # Coding Systems
-    (r'^admin_ui/', include('smart_ui_server.admin_ui.urls')),
+urlpatterns = patterns('')
 
-     # Everything to indivo
+if 'admin_ui' in settings.INSTALLED_APPS:
+    urlpatterns.extend(patterns('', 
+        (r'^admin_ui/', include('smart_ui_server.admin_ui.urls'))))
+
+urlpatterns.extend(
+    patterns('',
     (r'^', include('smart_ui_server.ui.urls')),
+    )   
 )
+
